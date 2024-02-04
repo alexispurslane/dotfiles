42b102ff8f34649d5b0651cbde9283dfb3874849
Alexis Purslane <alexispurslane@pm.me>
2024-02-03 20:00:13 -0500

    font was too big for some reason

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-font "JetBrains Mono-13"
+(setq doom-font "JetBrains Mono-12"
     doom-variable-pitch-font "Cantarell-12")
 (setq doom-theme 'doom-1337)
 
